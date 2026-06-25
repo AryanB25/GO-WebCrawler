cb8d815cd296bd5bf47a160f40859249e6f84cd0
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-06-25 14:42:09 -0600

    Renaming method names

--- internal/datastructures/queue.go
+++ internal/datastructures/queue.go
@@ -4,15 +4,15 @@ type Queue struct {
 	elements []string // stores the URL's
 }
 
-func newQueue() *Queue {
+func NewQueue() *Queue {
 	return &Queue{[]string{}} // creates a new queue and returns the address of the new queue
 }
 
-func (queue *Queue) enqueue(url string) {
+func (queue *Queue) Enqueue(url string) {
 	queue.elements = append(queue.elements, url) // adding the URL to the list of URL's
 }
 
-func (queue *Queue) dequeue() (string, bool) {
+func (queue *Queue) Dequeue() (string, bool) {
 	if len(queue.elements) == 0 { // if the queue is empty or has no URL's
 		return "", false
 	}
@@ -21,14 +21,14 @@ func (queue *Queue) dequeue() (string, bool) {
 	return front, true
 }
 
-func (queue *Queue) peek() string {
+func (queue *Queue) Peek() string {
 	return queue.elements[0] // peek at the first url present in the queue
 }
 
-func (queue *Queue) isEmpty() bool {
+func (queue *Queue) IsEmpty() bool {
 	return len(queue.elements) == 0 // checks if the queue is empty
 }
 
-func (queue *Queue) size() int {
+func (queue *Queue) Size() int {
 	return len(queue.elements) // returns the number of URL's in the queue
 }
