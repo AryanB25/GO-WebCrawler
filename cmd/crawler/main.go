70a5b37ba7fcc5aa1055316c8025c3e810649691
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-06-25 14:41:58 -0600

    Testing set structure

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,9 +1,16 @@
-package crawler
+package main
 
 import (
+	"GO-WebCrawler/internal/datastructures"
 	"fmt"
 )
 
 func main() {
-
+	sets := datastructures.NewSet()
+	sets.Add("https://en.wikipedia.org/wiki/Wikipedia")
+	sets.Add("https://aryanbhatt.com")
+	fmt.Println(sets.Size())
+	fmt.Println(*sets)
+	fmt.Println(sets.Contains("https://aryanbhatt.com"))
+	fmt.Println(sets.Contains("https://l.com"))
 }
