693ee9a8b57a2716564a4750f479b3af04436e1e
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-02 15:39:15 -0600

    Adding comments and documentation

--- internal/datastructures/set.go
+++ internal/datastructures/set.go
@@ -1,21 +1,21 @@
 package datastructures
 
-type Set struct {
+type Set struct { // structure of the set
 	elements map[string]bool
 }
 
-func NewSet() *Set {
-	return &Set{elements: map[string]bool{}}
+func NewSet() *Set { // create a new set
+	return &Set{elements: map[string]bool{}} // returns the address to a new set
 }
 
 func (set *Set) Add(url string) {
-	set.elements[url] = true
+	set.elements[url] = true // adds the url to the set
 }
 
 func (set *Set) Contains(url string) bool {
-	return set.elements[url]
+	return set.elements[url] // checks if the url is in the set
 }
 
 func (set *Set) Size() int {
-	return len(set.elements)
+	return len(set.elements) // returns the size of the set
 }
