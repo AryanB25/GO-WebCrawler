8801eda4145488cae09239d8a00d610856dbca01
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-03 12:09:32 -0600

    Single-threaded BFS Crawler

--- internal/scraper/crawler.go
+++ internal/scraper/crawler.go
@@ -1,16 +1,43 @@
 package scraper
 
-func crawl(seedURL string, maxPages int) {
-	// create queue
-	// create set
-	// enqueue seed URL
-	// loop while queue is not empty AND pages visited is under max
-	// dequeue next URL
-	// if already visited, skip
-	// mark as visited
-	// fetch page
-	// if fetch failed, skip
-	// tokenize page, get list of URLs
-	// enqueue each new URL
-	// increment page count
+import (
+	"GO-WebCrawler/internal/datastructures"
+	"fmt"
+)
+
+func Crawl(seedURL string, maxPages int) {
+	queue := datastructures.NewQueue() // create queue
+	set := datastructures.NewSet()     // create set
+	queue.Enqueue(seedURL)             // enqueue seed URL
+	counter := 0                       // keep track of the pages visited
+
+	for !queue.IsEmpty() && counter < maxPages {
+		currentURL, notEmpty := queue.Dequeue()
+
+		if !notEmpty { // if the queue is empty
+			continue
+		}
+
+		if set.Contains(currentURL) { // if the URL has been visited
+			continue
+		} else {
+			set.Add(currentURL) // add the URL to the set if not visited
+		}
+
+		fetchedData, err := FetchData(currentURL) // fetch the HTML data of the page
+
+		if err != nil { // if an error exists
+			continue
+		}
+
+		listURL := TokenizerURL(fetchedData) // tokenize the page (string HTML)
+
+		fmt.Println("URLs found:", len(listURL))
+
+		for _, url := range listURL {
+			queue.Enqueue(url) // enqueue the URL's into the queue
+		}
+
+		counter++ // increment page count
+	}
 }
