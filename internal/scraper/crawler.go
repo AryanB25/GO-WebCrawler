94d1dff60ce54eb862d96a6fb628ca2bef9ac296
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-02 19:34:07 -0600

    Crawler structure

---
+++ internal/scraper/crawler.go
@@ -0,0 +1,16 @@
+package scraper
+
+func crawl(seedURL string, maxPages int) {
+	// create queue
+	// create set
+	// enqueue seed URL
+	// loop while queue is not empty AND pages visited is under max
+	// dequeue next URL
+	// if already visited, skip
+	// mark as visited
+	// fetch page
+	// if fetch failed, skip
+	// tokenize page, get list of URLs
+	// enqueue each new URL
+	// increment page count
+}
