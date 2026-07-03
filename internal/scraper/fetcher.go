efb0558f9eac0cc04b5a85bbd44b6c5b5386ecbb
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-02 19:34:29 -0600

    Implementation - Data fetcher

---
+++ internal/scraper/fetcher.go
@@ -0,0 +1,31 @@
+package scraper
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func fetchData(url string) (string, error) {
+	connect, err := http.Get(url) // HTTP request
+
+	if err != nil { // if there are errors when establishing a HTTP request
+		return "", err
+	}
+
+	defer connect.Body.Close() // closes the HTTP connection after the function is over to prevent resource leak
+
+	if connect.StatusCode != http.StatusOK { // if the HTTP status is not ok
+		return "", fmt.Errorf("Unexpeted Status Code: %v", connect.Status)
+	}
+
+	rawData, err := io.ReadAll(connect.Body) // binary data of the url page
+
+	if err != nil { // if there are errors when reading the bytes of the url page
+		return "", err
+	}
+
+	formattedData := string(rawData) // binary converted to HTML string
+
+	return formattedData, nil // returns the formatted HTML string
+}
