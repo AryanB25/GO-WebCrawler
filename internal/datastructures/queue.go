e41dfb8be0f6d18dc157d443f54c85fbd42a3451
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-09 17:05:03 -0600

    Mutex addition

--- internal/datastructures/queue.go
+++ internal/datastructures/queue.go
@@ -1,34 +1,49 @@
 package datastructures
 
+import "sync"
+
 type Queue struct {
-	elements []string // stores the URL's
+	mutex    sync.Mutex // mutual exclusion lock
+	elements []string   // stores the URL's
 }
 
 func NewQueue() *Queue {
-	return &Queue{[]string{}} // creates a new queue and returns the address of the new queue
+	return &Queue{elements: []string{}} // creates a new queue and returns the address of the new queue
 }
 
 func (queue *Queue) Enqueue(url string) {
+	queue.mutex.Lock()                           // allow the function to perform it's independent function without race conditions
+	defer queue.mutex.Unlock()                   // unlock the resources after the function is over
 	queue.elements = append(queue.elements, url) // adding the URL to the list of URL's
 }
 
 func (queue *Queue) Dequeue() (string, bool) {
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
+
 	if len(queue.elements) == 0 { // if the queue is empty or has no URL's
 		return "", false
 	}
+
 	front := queue.elements[0]          // access the first URL of the queue
 	queue.elements = queue.elements[1:] // remove the first URL of the queue
 	return front, true
 }
 
 func (queue *Queue) Peek() string {
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
 	return queue.elements[0] // peek at the first url present in the queue
 }
 
 func (queue *Queue) IsEmpty() bool {
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
 	return len(queue.elements) == 0 // checks if the queue is empty
 }
 
 func (queue *Queue) Size() int {
+	queue.mutex.Lock()
+	defer queue.mutex.Unlock()
 	return len(queue.elements) // returns the number of URL's in the queue
 }
--- internal/datastructures/set.go
+++ internal/datastructures/set.go
@@ -1,6 +1,9 @@
 package datastructures
 
+import "sync"
+
 type Set struct { // structure of the set
+	mutex    sync.Mutex
 	elements map[string]bool
 }
 
@@ -9,13 +12,19 @@ func NewSet() *Set { // create a new set
 }
 
 func (set *Set) Add(url string) {
+	set.mutex.Lock()
+	defer set.mutex.Unlock()
 	set.elements[url] = true // adds the url to the set
 }
 
 func (set *Set) Contains(url string) bool {
+	set.mutex.Lock()
+	defer set.mutex.Unlock()
 	return set.elements[url] // checks if the url is in the set
 }
 
 func (set *Set) Size() int {
+	set.mutex.Lock()
+	defer set.mutex.Unlock()
 	return len(set.elements) // returns the size of the set
 }
