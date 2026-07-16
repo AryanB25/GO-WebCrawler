cb67ea441160bf1ffcf4115e4844dc3d1d5a1b08
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-16 17:18:15 -0600

    Best-First Testing

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -3,5 +3,5 @@ package main
 import "GO-WebCrawler/internal/scraper"
 
 func main() {
-	scraper.WorkerPool("http://books.toscrape.com", 20, 5)
+	scraper.WorkerPool("http://books.toscrape.com", 20, 5, "aryan")
 }
