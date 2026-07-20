005c297bce9f3f4c8de49f219b8581e475ad0d12
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-07-20 16:01:37 -0600

    Flag parsing in-line commands

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "GO-WebCrawler/internal/scraper"
+import (
+	"GO-WebCrawler/internal/index"
+	"GO-WebCrawler/internal/scraper"
+	"flag"
+	"fmt"
+)
 
 func main() {
-	scraper.WorkerPool("http://books.toscrape.com", 20, 5, "aryan")
+	searchFlag := flag.String("search", "", "search the crawled index for a keyword")
+	crawlTarget := flag.String("target", "books", "keyword to prioritize during crawling")
+	flag.Parse()
+	if *searchFlag != "" {
+		database, err := index.InitDB()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		listURL, err := index.SearchTerms(database, *searchFlag)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for _, current := range listURL {
+			fmt.Println(current)
+		}
+	} else {
+		scraper.WorkerPool("http://books.toscrape.com", 20, 5, *crawlTarget)
+	}
 }
